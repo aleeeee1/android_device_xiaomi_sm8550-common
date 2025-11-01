dc755abb01bd4ca89a78d032546198345c2a4ab9
aleeeee1 <pace.alessandro05@protonmail.com>
2025-11-01 11:23:34 +0000

    sm8550-common: literally copy paste sepolicy dir from other trees
    taken from https://github.com/lonelystar9x/device_xiaomi_sm8550-common_15/tree/Rising16/sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,7 +183,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # System properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
