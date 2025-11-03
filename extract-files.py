9b6bd40ac475d155baa8bb29f37b489547c8cc82
aleeeee1 <pace.alessandro05@protonmail.com>
2025-11-03 15:46:09 +0000

    sm8550-common: remove duplicated proprietary blob.
    also present on fuxi trees.

--- extract-files.py
+++ extract-files.py
@@ -89,7 +89,6 @@ blob_fixups: blob_fixups_user_type = {
     (
         'odm/lib64/libcamxcommonutils.so',
         'odm/lib64/hw/com.qti.chi.override.so',
-        'odm/lib64/hw/camera.xiaomi.so',
         'odm/lib64/libchifeature2.so',
         'odm/lib64/libmialgoengine.so'
     ): blob_fixup()
